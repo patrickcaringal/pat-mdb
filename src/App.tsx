fd00f90c33287c07f351195ab1c39aea3f6a7a13
Patrick Caringal <patrickcaringalreal@gmail.com>
2020-08-04 23:32:14 +0800

    setup routing and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch, Redirect } from 'react-router-dom';
+// import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
 
-const foo = (bar: any, baz: any) => {};
+// const foo = (bar: string, baz: any) => {};
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Switch>
+            <Route path="/1" component={() => <Home />} />
+            <Route path="/2" component={() => <h1>Test2</h1>} />
+            <Route path="/not-found" component={() => <h1>NotFound</h1>} />
+
+            <Redirect from="/" exact to="/1" />
+            <Redirect to="/not-found" />
+        </Switch>
+        // <div className="App">
+        //     <header className="App-header">
+        //         <img src={logo} className="App-logo" alt="logo" />
+        //         <p>
+        //             Edit <code>src/App.tsx</code> and save to reload.
+        //         </p>
+        //         <a
+        //             className="App-link"
+        //             href="https://reactjs.org"
+        //             target="_blank"
+        //             rel="noopener noreferrer"
+        //         >
+        //             Learn React
+        //         </a>
+        //     </header>
+        // </div>
     );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC<{}> = () => {
+    return (
+        <>
+            <h1 style={{ fontFamily: "'Source Sans Pro', Arial, sans-serif" }}>This is HOME</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis, assumenda?</p>
+        </>
+    );
+};
+
+export default Home;
