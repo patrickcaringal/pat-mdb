1f4caf8597434b4c0eb481a6e085d2efe4daf818
Patrick Caringal <patrickcaringalreal@gmail.com>
2020-08-05 23:13:50 +0800

    materialui & appbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,48 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 
-const Home: React.FC<{}> = () => {
+function ElevationScroll({ children }: { children: React.ReactElement }) {
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0
+    });
+
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0
+    });
+}
+
+const Home: React.FC<{}> = (props) => {
     return (
-        <>
-            <h1 style={{ fontFamily: "'Source Sans Pro', Arial, sans-serif" }}>This is HOME</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis, assumenda?</p>
-        </>
+        <React.Fragment>
+            <CssBaseline />
+            <ElevationScroll>
+                <AppBar>
+                    <Toolbar>
+                        <Typography variant="h6">PAT MDb</Typography>
+                    </Toolbar>
+                </AppBar>
+            </ElevationScroll>
+            <Toolbar />
+            <Container>
+                <Box my={2}>
+                    {[...new Array(50)]
+                        .map(
+                            () => `Cras mattis consectetur purus sit amet fermentum.
+                            Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+                            Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+                        )
+                        .join('\n')}
+                </Box>
+            </Container>
+        </React.Fragment>
     );
 };
 
