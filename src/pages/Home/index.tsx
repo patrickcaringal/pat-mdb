b3f410dc8b1f9688995a9e6054b32e2e927afddd
Patrick Caringal <patrickcaringalreal@gmail.com>
2020-08-23 23:43:23 +0800

    search button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,35 +10,24 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Typography from '@material-ui/core/Typography';
 
 import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
-import DirectionsIcon from '@material-ui/icons/Directions';
 
 import Card from './Card';
 import { Popular as PopularMovies, Genres } from './mockData';
 import landingImg from '../../asset/img/landing-bg.jpg';
-// import useIntersect from '../../customhooks/useIntersect';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        padding: '2px 4px',
+    searchCont: {
         display: 'flex',
-        alignItems: 'center',
-        width: 400
+        background: 'white',
+        width: '65%',
+        paddingLeft: '15px',
+        borderRadius: '30px'
     },
-    input: {
-        marginLeft: theme.spacing(1),
-        flex: 1
-    },
-    iconButton: {
-        padding: 10
-    },
-    divider: {
-        height: 28,
-        margin: 4
+    seachBtn: {
+        width: '75px',
+        '&:hover': { background: 'transparent' }
     }
 }));
 
@@ -82,49 +71,21 @@ const Home: React.FC = (props) => {
                             Keep track of your favorite shows.
                         </Typography>
                     </Box>
-                    <Box display="flex" width="70%">
-                        {/* <FormControl variant="filled" fullWidth>
-                            <InputLabel style={{ paddingLeft: '20px' }}>
-                                Search for a movie, tv show series, person
-                            </InputLabel>
+
+                    <Paper className={classes.searchCont}>
+                        <FormControl variant="filled" fullWidth>
+                            <InputLabel>Search for a movie, tv show series, person</InputLabel>
                             <FilledInput
                                 value={searchQuery}
                                 disableUnderline
                                 onChange={(e) => setSearchQuery(e.target.value)}
-                                style={{
-                                    background: 'white',
-                                    borderRadius: '30px',
-                                    paddingLeft: '16px'
-                                }}
+                                style={{ background: 'none' }}
                             />
-                        </FormControl> */}
-
-                        <Paper component="form" className={classes.root}>
-                            <IconButton className={classes.iconButton} aria-label="menu">
-                                <MenuIcon />
-                            </IconButton>
-                            <InputBase
-                                className={classes.input}
-                                placeholder="Search Google Maps"
-                                inputProps={{ 'aria-label': 'search google maps' }}
-                            />
-                            <IconButton
-                                type="submit"
-                                className={classes.iconButton}
-                                aria-label="search"
-                            >
-                                <SearchIcon />
-                            </IconButton>
-                            <Divider className={classes.divider} orientation="vertical" />
-                            <IconButton
-                                color="primary"
-                                className={classes.iconButton}
-                                aria-label="directions"
-                            >
-                                <DirectionsIcon />
-                            </IconButton>
-                        </Paper>
-                    </Box>
+                        </FormControl>
+                        <IconButton className={classes.seachBtn} aria-label="search">
+                            <SearchIcon />
+                        </IconButton>
+                    </Paper>
                 </Box>
             </Grid>
 
