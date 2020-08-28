c05704dd5c8fb4ee179d8de248094aea7f4c5393
Patrick Caringal <patrickcaringalreal@gmail.com>
2020-08-28 22:45:32 +0800

    [search] movie card component with intersection observer

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import { parse as QSParse } from 'query-string';
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -14,47 +14,24 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-
 import FilledInput from '@material-ui/core/FilledInput';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 
-const useStyles = makeStyles((theme) => ({
-    demo: {
-        backgroundColor: theme.palette.background.paper
-    },
-    root: {
-        display: 'flex',
-        marginBottom: '18px'
-    },
-    cover: {
-        width: 94,
-        height: 141
-    },
-    content: {
-        display: 'flex',
-        flexDirection: 'column',
-        flex: '1',
-        justifyContent: 'space-between'
-    }
-}));
+import MovieCard from './MovieCard';
+
+// const useStyles = makeStyles({});
 
 interface SearchProps extends RouteComponentProps {}
 
 const SearchPage: React.FC<SearchProps> = (props) => {
-    const classes = useStyles();
+    // const classes = useStyles();
 
-    const [searchQuery, setSearchQuery] = useState<string>('');
+    const searchTxt: string = QSParse(props.location.search).query as string;
 
-    useEffect(() => {
-        const searchTxt: string = QSParse(props.location.search).query as string;
-        setSearchQuery(searchTxt);
-    }, []);
+    const [searchQuery, setSearchQuery] = useState<string>(searchTxt);
 
     return (
         <Box display="flex" p={3}>
@@ -114,32 +91,12 @@ const SearchPage: React.FC<SearchProps> = (props) => {
                     <Grid item xs={9}>
                         <Box display="flex" flexDirection="column" ml={3}>
                             {[...new Array(10)].map(() => (
-                                <Card className={classes.root}>
-                                    <CardMedia
-                                        className={classes.cover}
-                                        image="https://via.placeholder.com/94x141/767c77/fabea7"
-                                        title="Live from space album cover"
-                                    />
-                                    <CardContent
-                                        className={classes.content}
-                                        style={{ paddingBottom: 10, paddingTop: 10 }}
-                                    >
-                                        <Box>
-                                            <Typography variant="h6" style={{ fontWeight: 600 }}>
-                                                Movie title
-                                            </Typography>
-                                            <Typography color="textSecondary">
-                                                July 10, 2020
-                                            </Typography>
-                                        </Box>
-                                        <Typography className="line-clamp">
-                                            Lorem ipsum, dolor sit amet consectetur adipisicing
-                                            elit. Amet quae quibusdam nam veniam laborum iusto
-                                            magnam inventore voluptate officia voluptatibus! Sit,
-                                            vel omnis enim ex sunt dolores officiis velit molestiae.
-                                        </Typography>
-                                    </CardContent>
-                                </Card>
+                                <MovieCard
+                                    image="https://via.placeholder.com/94x141/767c77/fabea7"
+                                    title="Movie title"
+                                    subtitle="July 10, 2020"
+                                    description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet quae quibusdam nam veniam laborum iusto magnam inventore voluptate officia voluptatibus! Sit, vel omnis enim ex sunt dolores officiis velit molestiae."
+                                />
                             ))}
                         </Box>
                     </Grid>
