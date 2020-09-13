022b1a909a86efea962e5ff373894e7336a1f955
Patrick Caringal <patrickcaringalreal@gmail.com>
2020-09-13 15:10:16 +0800

    [movie catalog][ui] separate sidebar component + movie category header

---
+++ src/pages/Movie/Catalog/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+import Sidebar from './Sidebar';
+
+interface MatchParams {
+    id: string;
+}
+
+interface MovieProps extends RouteComponentProps<MatchParams> {}
+
+const Catalog: React.FC<MovieProps> = ({ match }) => {
+    const { id: movieCategory } = match.params;
+
+    const urlToWord = (str: string) => str.replace('-', ' '); // helper
+
+    return (
+        <Box display="flex" mx={4} my={3}>
+            <Container disableGutters maxWidth="lg">
+                <Typography
+                    variant="h5"
+                    style={{ fontWeight: 600, marginBottom: 16, textTransform: 'capitalize' }}
+                >
+                    {urlToWord(movieCategory)}
+                </Typography>
+
+                <Grid container>
+                    {/* Sort & Filter sidebar */}
+                    <Grid item xs={3}>
+                        <Sidebar />
+                    </Grid>
+
+                    {/* Catalog */}
+                    <Grid item xs={9}>
+                        <Box
+                            display="flex"
+                            flexDirection="column"
+                            ml={3}
+                            style={{ border: '1px solid #ccc' }}
+                        ></Box>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
+    );
+};
+
+export default withRouter(Catalog);
