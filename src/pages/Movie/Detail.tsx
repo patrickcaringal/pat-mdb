215f5014acfa6d29a2f5178cf9b5f223a43aa19c
Patrick Caringal <patrickcaringalreal@gmail.com>
2020-09-22 22:26:50 +0800

    [movie detail] banner contents

--- src/pages/Movie/Detail.tsx
+++ src/pages/Movie/Detail.tsx
@@ -19,7 +19,8 @@ const useStyles = makeStyles({
             'linear-gradient(to right, rgba(14.12%, 14.51%, 16.86%, 1.00) 150px, rgba(22.35%, 22.35%, 22.35%, 0.84) 100%)'
     },
     poster: {
-        width: 300
+        width: 300,
+        height: 450
     }
 });
 
@@ -37,29 +38,43 @@ const MoivieDetail: React.FC<MovieDetailProps> = ({ match }) => {
             <Box display="flex" flex="1" className={classes.backdropOverlay}>
                 <Container disableGutters maxWidth="lg">
                     <Box display="flex" flexDirection="row" p={4}>
-                        <img
-                            src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/MoEKaPFHABtA1xKoOteirGaHl1.jpg"
-                            alt="PAT MDb"
-                            className={classes.poster}
-                        />
+                        {/* style={{ height: 570 + 46 }} */}
+                        <Box display="flex" flexDirection="column">
+                            <img
+                                src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/MoEKaPFHABtA1xKoOteirGaHl1.jpg"
+                                alt="PAT MDb"
+                                className={classes.poster}
+                            />
+                            {/* <Box my={3} style={{ height: 68, background: 'khaki' }}></Box> */}
+                        </Box>
                         <Box
                             display="flex"
                             flex="1"
                             flexDirection="column"
-                            justifyContent="center"
+                            // justifyContent="center"
                             pl={5}
-                            style={{ border: '1px solid royalblue' }}
                         >
-                            {/* <h1>Detail {match.params.id}</h1> */}
-                            <Typography
-                                variant="h3"
-                                style={{ fontWeight: 700 }}
-                                // className={classes.bannerHeader}
-                            >
+                            <Typography variant="h3" style={{ fontWeight: 700 }}>
                                 Money Heist
                             </Typography>
-                            <Typography variant="h6">Lorem ipsum dolor sit amet.</Typography>
-                            <Typography variant="h6" style={{ fontWeight: 700 }}>
+                            <Typography>Action, Crime, Drama - 2h 4m</Typography>
+
+                            <Box display="flex" flexDirection="column" mt={1} mb={2}>
+                                <Box display="flex" flexDirection="row" alignItems="center">
+                                    <Typography variant="h4" style={{ fontWeight: 700 }}>
+                                        8.4
+                                    </Typography>
+                                    <Typography variant="h6">&nbsp;/&nbsp;10</Typography>
+                                </Box>
+                                <Typography>15,081 votes</Typography>
+                            </Box>
+
+                            <Typography
+                                style={{ fontStyle: 'italic', fontWeight: 600, marginBottom: 6 }}
+                            >
+                                Lorem ipsum dolor sit amet.
+                            </Typography>
+                            <Typography variant="h6" style={{ fontWeight: 700, marginBottom: 6 }}>
                                 Overview
                             </Typography>
                             <Typography>
@@ -72,6 +87,27 @@ const MoivieDetail: React.FC<MovieDetailProps> = ({ match }) => {
                                 hostages in their power, to find out whether their suicide wager
                                 will lead to everything or nothing.
                             </Typography>
+
+                            <Box display="flex" flexWrap="wrap" mt={4}>
+                                <Box flex="4">
+                                    <Typography style={{ fontWeight: 700 }}>Director</Typography>
+                                    <Typography>Joe Russo</Typography>
+                                </Box>
+                                <Box flex="4">
+                                    <Typography style={{ fontWeight: 700 }}>
+                                        Release date
+                                    </Typography>
+                                    <Typography>April 24, 2019</Typography>
+                                </Box>
+                                <Box flex="4">
+                                    <Typography style={{ fontWeight: 700 }}>Budget</Typography>
+                                    <Typography>$356,000,000.00</Typography>
+                                </Box>
+                                <Box flex="4">
+                                    <Typography style={{ fontWeight: 700 }}>Revenue</Typography>
+                                    <Typography>$2,797,800,564.00</Typography>
+                                </Box>
+                            </Box>
                         </Box>
                     </Box>
                 </Container>
