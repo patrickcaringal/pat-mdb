460f49dd28089771db9ffb318c91b92d4827c731
Patrick Caringal <patrickcaringalreal@gmail.com>
2020-09-23 22:54:28 +0800

    [movie detail] cast, keywords section

--- src/pages/Movie/Detail.tsx
+++ src/pages/Movie/Detail.tsx
@@ -6,6 +6,12 @@ import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+
+import Chip from '@material-ui/core/Chip';
+
 const useStyles = makeStyles({
     backdrop: {
         background:
@@ -20,7 +26,37 @@ const useStyles = makeStyles({
     },
     poster: {
         width: 300,
-        height: 450
+        height: 450,
+        borderRadius: 8
+    },
+    movieConent: {
+        // background: 'khaki',
+        // height: 500
+    },
+    leftSidebar: {
+        // border: '1px solid red',
+        // boxSizing: 'border-box',
+        width: 260,
+        minWidth: 260
+    },
+
+    cardCont: {
+        minWidth: 138,
+        marginRight: 14
+    },
+    cardImg: {
+        height: 175
+    },
+    cardContent: {
+        '&.MuiCardContent-root:last-child': {
+            paddingBottom: 16
+        }
+    },
+    title: {
+        fontWeight: 600
+    },
+    subtitle: {
+        color: '#696969'
     }
 });
 
@@ -34,87 +70,163 @@ const MoivieDetail: React.FC<MovieDetailProps> = ({ match }) => {
     const classes = useStyles();
 
     return (
-        <Box display="flex" className={classes.backdrop}>
-            <Box display="flex" flex="1" className={classes.backdropOverlay}>
-                <Container disableGutters maxWidth="lg">
-                    <Box display="flex" flexDirection="row" p={4}>
-                        {/* style={{ height: 570 + 46 }} */}
-                        <Box display="flex" flexDirection="column">
-                            <img
-                                src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/MoEKaPFHABtA1xKoOteirGaHl1.jpg"
-                                alt="PAT MDb"
-                                className={classes.poster}
-                            />
-                            {/* <Box my={3} style={{ height: 68, background: 'khaki' }}></Box> */}
+        <>
+            <Box display="flex" className={classes.backdrop}>
+                <Box display="flex" flex="1" className={classes.backdropOverlay}>
+                    <Container disableGutters maxWidth="lg">
+                        <Box display="flex" flexDirection="row" p={4}>
+                            {/* style={{ height: 570 + 46 }} */}
+                            <Box display="flex" flexDirection="column">
+                                <img
+                                    src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/MoEKaPFHABtA1xKoOteirGaHl1.jpg"
+                                    alt="PAT MDb"
+                                    className={classes.poster}
+                                />
+                            </Box>
+                            <Box display="flex" flex="1" flexDirection="column" pl={5}>
+                                <Typography variant="h3" style={{ fontWeight: 700 }}>
+                                    Money Heist
+                                </Typography>
+                                <Typography>Action, Crime, Drama - 2h 4m</Typography>
+
+                                <Box display="flex" flexDirection="column" mt={1} mb={2}>
+                                    <Box display="flex" flexDirection="row" alignItems="center">
+                                        <Typography variant="h4" style={{ fontWeight: 700 }}>
+                                            8.4
+                                        </Typography>
+                                        <Typography variant="h6">&nbsp;/&nbsp;10</Typography>
+                                    </Box>
+                                    <Typography>15,081 votes</Typography>
+                                </Box>
+
+                                <Typography
+                                    style={{
+                                        fontStyle: 'italic',
+                                        fontWeight: 600,
+                                        marginBottom: 6
+                                    }}
+                                >
+                                    Lorem ipsum dolor sit amet.
+                                </Typography>
+                                <Typography
+                                    variant="h6"
+                                    style={{ fontWeight: 700, marginBottom: 6 }}
+                                >
+                                    Overview
+                                </Typography>
+                                <Typography>
+                                    To carry out the biggest heist in history, a mysterious man
+                                    called The Professor recruits a band of eight robbers who have a
+                                    single characteristic: none of them has anything to lose. Five
+                                    months of seclusion - memorizing every step, every detail, every
+                                    probability - culminate in eleven days locked up in the National
+                                    Coinage and Stamp Factory of Spain, surrounded by police forces
+                                    and with dozens of hostages in their power, to find out whether
+                                    their suicide wager will lead to everything or nothing.
+                                </Typography>
+
+                                <Box display="flex" flexWrap="wrap" mt={4}>
+                                    <Box flex="4">
+                                        <Typography style={{ fontWeight: 700 }}>
+                                            Director
+                                        </Typography>
+                                        <Typography>Joe Russo</Typography>
+                                    </Box>
+                                    <Box flex="4">
+                                        <Typography style={{ fontWeight: 700 }}>
+                                            Release date
+                                        </Typography>
+                                        <Typography>April 24, 2019</Typography>
+                                    </Box>
+                                    <Box flex="4">
+                                        <Typography style={{ fontWeight: 700 }}>Budget</Typography>
+                                        <Typography>$356,000,000.00</Typography>
+                                    </Box>
+                                    <Box flex="4">
+                                        <Typography style={{ fontWeight: 700 }}>Revenue</Typography>
+                                        <Typography>$2,797,800,564.00</Typography>
+                                    </Box>
+                                </Box>
+                            </Box>
                         </Box>
+                    </Container>
+                </Box>
+            </Box>
+
+            <Container disableGutters maxWidth="lg">
+                <Box
+                    display="flex"
+                    flexDirection="row"
+                    mt={4}
+                    mx={4}
+                    className={classes.movieConent}
+                >
+                    <Box
+                        display="flex"
+                        flexDirection="column"
+                        flex="1"
+                        style={{ overflow: 'hidden' }}
+                    >
+                        <Typography variant="h5" style={{ fontWeight: 600 }}>
+                            Cast
+                        </Typography>
+
                         <Box
                             display="flex"
-                            flex="1"
-                            flexDirection="column"
-                            // justifyContent="center"
-                            pl={5}
+                            flexDirection="row"
+                            mt={2}
+                            mb={4}
+                            pb={2}
+                            style={{ width: '100%', overflow: 'auto' }}
                         >
-                            <Typography variant="h3" style={{ fontWeight: 700 }}>
-                                Money Heist
-                            </Typography>
-                            <Typography>Action, Crime, Drama - 2h 4m</Typography>
+                            {[...Array(8)].map((i) => (
+                                <Card className={classes.cardCont}>
+                                    <CardMedia
+                                        className={classes.cardImg}
+                                        // https://image.tmdb.org/t/p/w138_and_h175_face/wZkK15LnloSAhzs1jxI3AZbR6f0.jpg
+                                        image="https://image.tmdb.org/t/p/w185/2TGPhdpRC5wjdFEJqnLYiN5kbwg.jpg"
+                                        title="title"
+                                    />
+                                    <CardContent className={classes.cardContent}>
+                                        <Typography className={classes.title}>
+                                            Úrsula Corberó
+                                        </Typography>
+                                        <Typography variant="body2" className={classes.subtitle}>
+                                            Tokyo
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            ))}
+                        </Box>
 
-                            <Box display="flex" flexDirection="column" mt={1} mb={2}>
-                                <Box display="flex" flexDirection="row" alignItems="center">
-                                    <Typography variant="h4" style={{ fontWeight: 700 }}>
-                                        8.4
-                                    </Typography>
-                                    <Typography variant="h6">&nbsp;/&nbsp;10</Typography>
-                                </Box>
-                                <Typography>15,081 votes</Typography>
+                        <Typography variant="h5" style={{ fontWeight: 600 }}>
+                            Media
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" className={classes.leftSidebar} ml={3}>
+                        <Box display="flex" flexDirection="column">
+                            <Typography style={{ fontWeight: 700 }}>Keywords</Typography>
+                            <Box display="flex" flexWrap="wrap" mt={2} mb={4}>
+                                {[...Array(8)].map((i) => (
+                                    <Chip
+                                        // key={key}
+                                        label="Keyword"
+                                        onClick={() => {}}
+                                        // variant={isGenreSelected(key) ? 'default' : 'outlined'}
+                                        size="small"
+                                        style={{ margin: 4 }}
+                                    />
+                                ))}
                             </Box>
 
-                            <Typography
-                                style={{ fontStyle: 'italic', fontWeight: 600, marginBottom: 6 }}
-                            >
-                                Lorem ipsum dolor sit amet.
-                            </Typography>
-                            <Typography variant="h6" style={{ fontWeight: 700, marginBottom: 6 }}>
-                                Overview
-                            </Typography>
-                            <Typography>
-                                To carry out the biggest heist in history, a mysterious man called
-                                The Professor recruits a band of eight robbers who have a single
-                                characteristic: none of them has anything to lose. Five months of
-                                seclusion - memorizing every step, every detail, every probability -
-                                culminate in eleven days locked up in the National Coinage and Stamp
-                                Factory of Spain, surrounded by police forces and with dozens of
-                                hostages in their power, to find out whether their suicide wager
-                                will lead to everything or nothing.
-                            </Typography>
-
-                            <Box display="flex" flexWrap="wrap" mt={4}>
-                                <Box flex="4">
-                                    <Typography style={{ fontWeight: 700 }}>Director</Typography>
-                                    <Typography>Joe Russo</Typography>
-                                </Box>
-                                <Box flex="4">
-                                    <Typography style={{ fontWeight: 700 }}>
-                                        Release date
-                                    </Typography>
-                                    <Typography>April 24, 2019</Typography>
-                                </Box>
-                                <Box flex="4">
-                                    <Typography style={{ fontWeight: 700 }}>Budget</Typography>
-                                    <Typography>$356,000,000.00</Typography>
-                                </Box>
-                                <Box flex="4">
-                                    <Typography style={{ fontWeight: 700 }}>Revenue</Typography>
-                                    <Typography>$2,797,800,564.00</Typography>
-                                </Box>
-                            </Box>
+                            <Typography style={{ fontWeight: 700 }}>You may also like</Typography>
                         </Box>
                     </Box>
-                </Container>
-            </Box>
-        </Box>
+                </Box>
+            </Container>
+        </>
     );
-    // <h1>Detail {match.params.id}</h1>;
 };
 
 export default withRouter(MoivieDetail);
